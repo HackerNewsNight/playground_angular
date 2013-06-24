bd9513945d9b4e022388d84d7166cf6cf2319c6c
Darren Manners <darrenmanners@gmail.com>
2013-06-24 21:16:29 +1000

    + Created Beer service for SRP.. (will be used to call data from either mongodb or couch db.. not sure yet. )

--- app/js/controllers/BeerController.js
+++ app/js/controllers/BeerController.js
@@ -1,101 +1,15 @@
 'use strict';
 
 beerApp.controller('BeerController',
-    function BeerController($scope) {
+    function BeerController($scope, beerData) {
 
         // Beer Details
-        $scope.beer = {
-            id: 1,
-            name: "Lorum Ipsum",
-            category: {
-                name: "Duis sed ligula "
-            },
-            brewer: {
-                name: "Etiam luctus",
-                address: {
-                    addressline1: "1 eleifend St",
-                    addressline2: "",
-                    state: "ullamcorper",
-                    postcode: 9999,
-                    country: "Mauris"
-                }
-            },
-            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sed ligula vitae erat viverra molestie. Sed accumsan, est ac ullamcorper sodales, lorem eros aliquet risus,",
-            images: [ "./img/beers/lipsum.gif"],
-            alcoholPercentage: 4.011
-        };
+        $scope.beer = beerData.sampleData;
         $scope.beer.isReadonly = false;
-        /* todo: create access based on this information */
         $scope.beer.editMode = false;
-        /* todo: create input page based on this being in edit mode */
-
 
         // Sample Beers List...
-        $scope.beers = [
-            {
-                id: 2,
-                name: "Sol's Hocus Pocus",
-                category: {
-                    name: "Irish Lager"
-                },
-                brewer: {
-                    name: "Pixie Wonder",
-                    address: {
-                        addressline1: "1 Sussex St",
-                        addressline2: "",
-                        state: "NSW",
-                        postcode: 2000,
-                        country: "Australia"
-                    }
-                },
-                description: "Our take on a classic summer ale.  A toast to weeds, rays, and summer haze.  A light, crisp ale for mowing lawns, hitting lazy fly balls, and communing with nature, Hocus Pocus is offered up as a summer sacrifice to clodless days. Its malty sweetness ...",
-                upVoteCount: 0,
-                images: [ "./img/beers/sol.jpg" ],
-                alcoholPercentage: 8.05
-            },
-            {
-                id: 3,
-                name: "Fat Yak",
-                category: {
-                    name: "Oil Ale"
-                },
-                brewer: {
-                    name: "Big Al's Lab",
-                    address: {
-                        addressline1: "1 Sussex St",
-                        addressline2: "",
-                        state: "NSW",
-                        postcode: 2000,
-                        country: "Australia"
-                    }
-                },
-                description: "Is it dark in here. ",
-                upVoteCount: 0,
-                images: [ "./img/beers/fat_yak.jpg" ],
-                alcoholPercentage: 15.05
-            },
-            {
-                id: 1,
-                name: "Coopers Pale Ale",
-                category: {
-                    name: "Whacky Ale"
-                },
-                brewer: {
-                    name: "Backyard Shed",
-                    address: {
-                        addressline1: "88 North East St",
-                        addressline2: "",
-                        state: "ACT",
-                        postcode: 2000,
-                        country: "Australia"
-                    }
-                },
-                description: "Cloudy...",
-                upVoteCount: 0,
-                images: [ "./img/beers/coopers.jpg" ],
-                alcoholPercentage: 3.05
-            }
-        ];
+        $scope.beers = beerData.beers;
         $scope.beers.isVisible = true;
         $scope.beers.sortOrder = "name";
 
